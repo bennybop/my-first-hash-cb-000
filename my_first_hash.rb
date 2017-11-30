8efc9c665de7da69b234cbfbc2acf98c3550bc21
bennybop <bennybop74@yahoo.com>
2017-11-30 02:49:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
-shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
@@ -41,4 +41,3 @@ shipping_manifest["gun powder"] = 4
   # return the shipping_manifest hash below
 shipping_manifest
 end
-
